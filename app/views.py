d9a9bb377c7cc23b3a6b01bd07afef3b0eac1f11
atharva8830 <atharavasangle12@gmail.com>
2026-02-17 17:28:41 +0530

    initial commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def home_view (request):
+    return render (request , 'home.html')
+
+def java_views(request):
+    return render (request , 'java.html')
+
+def apti_views(request):
+    return render (request , 'apti.html')
+
+@login_required
+def django_views(request):
+    return render (request , 'django.html')
